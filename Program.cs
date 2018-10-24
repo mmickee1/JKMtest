cf855ac6b90aacd2c0925ecc63c9a9c485af1007
Mikael M <mikael_96@live.com>
2018-10-24 10:45:25 +0300

    update

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
